0fd051b8830c2ce5f511cc98e82a67fcf30afe2b
zocker-160 <zocker1600@posteo.net>
2021-07-14 22:24:47 +0200

    fix imports

--- xcom_232i/__init__.py
+++ xcom_232i/__init__.py
@@ -1,4 +1,2 @@
-
-if __name__ == '__main__':
-    from .XcomRS232 import RS232_IO as XcomRS232
-    from . import constants as XcomC
+from .XcomRS232 import RS232_IO as XcomRS232
+from . import constants as XcomC
