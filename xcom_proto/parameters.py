bae1e0841d1c04aa2e5c16501eec90405f75d7e5
zocker_160 <zocker1600@posteo.net>
2023-12-12 23:06:56 +0100

    add more parameters to Dataset
    thanks to @JeffersM1

--- xcom_proto/parameters.py
+++ xcom_proto/parameters.py
@@ -169,14 +169,17 @@ ERROR_CODES = {
 
 class Dataset:
 
+    # Xtender parameters (Writable)
     SMART_BOOST_ALLOWED = Datapoint(1126, "SMART_BOOST_ALLOWED", TYPE_BOOL)
     BATTERY_CHARGE_CURR = Datapoint(1138, "BATTERY_CHARGE_CURR", TYPE_FLOAT)
     SMART_BOOST_LIMIT = Datapoint(1607, "SMART_BOOST_LIMIT", TYPE_FLOAT)
 
     PARAMS_SAVED_IN_FLASH = Datapoint(1550, "PARAMS_SAVED_IN_FLASH", TYPE_BOOL)
 
+    # RCC / Xcom-232i parameters (Not SCOM accessible, do not use)
     USER_LEVEL = Datapoint(5012, "USER_LEVEL", TYPE_SHORT_ENUM)
 
+    # Xtender infos (Read only)
     AC_ENERGY_IN_CURR_DAY = Datapoint(3081, "AC_POWER_IN_CURR_DAY", TYPE_FLOAT, "kWh")
     AC_ENERGY_IN_PREV_DAY = Datapoint(3080, "AC_POWER_IN_PREV_DAY", TYPE_FLOAT, "kWh")
     AC_ENERGY_OUT_CURR_DAY = Datapoint(3083, "AC_ENERGY_OUT_CURR_DAY", TYPE_FLOAT, "kWh")
@@ -187,13 +190,27 @@ class Dataset:
     AC_POWER_OUT = Datapoint(3136, "AC_POWER_OUT", TYPE_FLOAT, "kW")
     AC_VOLTAGE_IN = Datapoint(3011, "AC_VOLTAGE_IN", TYPE_FLOAT, "V")
     AC_VOLTAGE_OUT = Datapoint(3021, "AC_VOLTAGE_OUT", TYPE_FLOAT, "V")
+    AC_CURRENT_IN = Datapoint(3012, "AC_CURRENT_IN", TYPE_FLOAT, "A")
+    AC_CURRENT_OUT = Datapoint(3022, "AC_CURRENT_OUT", TYPE_FLOAT, "A")
+    BATT_CYCLE_PHASE_XT = Datapoint(3010, "BATT_CYCLE_PHASE_XT", TYPE_SHORT_ENUM)
 
+    # Xcom-CAN BMS parameters (Writable)
+    SOC_LEVEL_FOR_BACKUP = Datapoint(6062, "SOC_LEVEL_FOR_BACKUP", TYPE_FLOAT)
+    SOC_LEVEL_FOR_GRID_FEEDING = Datapoint(6063, "SOC_LEVEL_FOR_GRID_FEEDING", TYPE_FLOAT)
+
+    # BSP infos (Read only)
     BATT_VOLTAGE = Datapoint(7000, "BATT_VOLTAGE", TYPE_FLOAT, "V")
     BATT_CURRENT = Datapoint(7001, "BATT_CURRENT", TYPE_FLOAT, "A")
     BATT_SOC = Datapoint(7032, "BATT_SOC", TYPE_FLOAT, "%")
     BATT_TEMP = Datapoint(7029, "BATT_TEMP", TYPE_FLOAT, "°C")
     BATT_CYCLE_PHASE = Datapoint(11038, "BATT_CYCLE_PHASE", TYPE_SHORT_ENUM)
+    BATT_POWER = Datapoint(7003, "BATT_POWER", TYPE_FLOAT, "W")
+    BATT_CHARGE = Datapoint(7007, "BATT_CHARGE", TYPE_FLOAT, "Ah")
+    BATT_DISCHARGE = Datapoint(7008, "BATT_DISCHARGE", TYPE_FLOAT, "Ah")
+    BATT_CHARGE_PREV_DAY = Datapoint(7009, "BATT_CHARGE_PREV_DAY", TYPE_FLOAT, "Ah")
+    BATT_DISCHARGE_PREV_DAY = Datapoint(7010, "BATT_DISCHARGE_PREV_DAY", TYPE_FLOAT, "Ah")
 
+    # VarioTrack infos (Read only)
     PV_VOLTAGE = Datapoint(11041 ,"PV_VOLTAGE", TYPE_FLOAT, "V")
     PV_POWER = Datapoint(11043, "PV_POWER", TYPE_FLOAT, "W")
     PV_ENERGY_CURR_DAY = Datapoint(11007, "PV_ENERGY_CURR_DAY", TYPE_FLOAT, "kWh")
@@ -205,7 +222,13 @@ class Dataset:
     PV_OPERATION_MODE = Datapoint(11016, "PV_OPERATION_MODE", TYPE_SHORT_ENUM)
     PV_NEXT_EQUAL = Datapoint(11037, "PV_NEXT_EQUAL", TYPE_FLOAT, "d")
 
+    # VarioTrack parameters (Writable)
     FORCE_NEW_CYCLE = Datapoint(10029, "FORCE_NEW_CYCLE", TYPE_SINT)
+
+    # VarioString infos (Read only)
+    VS_PV_POWER = Datapoint(15010, "VS_PV_POWER", TYPE_FLOAT, "kW")
+    VS_PV_PROD = Datapoint(15017, "VS_PV_PROD", TYPE_FLOAT, "kWh")
+    VS_PV_ENERGY_PREV_DAY = Datapoint(15026, "VS_PV_ENERGY_PREV_DAY", TYPE_FLOAT, "kWh")
 
     @staticmethod
     def getParamByID(id: int) -> Datapoint:
